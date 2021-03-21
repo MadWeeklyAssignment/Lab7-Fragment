1301c46a3ad9cbccaea5e437d99eadfe599ccc0b
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-21 21:18:56 +0545

    added rating bar

--- FragmentExample1_challenge/app/src/main/java/com/example/fragmentexample1_challenge/ui/main/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.fragmentexample1_challenge.ui.main;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
